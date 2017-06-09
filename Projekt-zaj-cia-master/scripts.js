f3f46fc4c8382a3fadd46b26cc747ed50021b83f
DESKTOP-F4NIAD1\janic <janicus321@gmail.com>
2017-06-09 23:49:27 +0200

    commit 7

--- Projekt-zaj-cia-master/scripts.js
+++ Projekt-zaj-cia-master/scripts.js
@@ -1,7 +1,25 @@
 
 function show_news() {
-    document.getElementById("show_content").innerHTML = "Lorem ipsum dolor sit amet,"; 
+    document.getElementById("show_content").innerHTML = "Nam sit amet ligula eu neque euismod rhoncus quis at est. Morbi sagittis lectus vitae ullamcorper vehicula. Cras vel lorem leo. Nullam id commodo ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mi neque, dictum eget tincidunt ut, vestibulum blandit ipsum. Praesent tempor rutrum ipsum eu faucibus. Nam non lacinia arcu, non porta dui. Donec ultricies sollicitudin magna, a gravida erat. Aliquam dignissim convallis lacus, eu semper mi lobortis vel. Quisque mattis nisl nulla, quis semper elit viverra vel. Vestibulum id nibh molestie, porttitor dui eget, fermentum neque. Fusce condimentum lobortis aliquet."; 
+}
+
+function show_news2() {
+    document.getElementById("show_content2").innerHTML = "Ut finibus diam at dui congue, eu fermentum ex vehicula. Donec nec tortor varius, efficitur purus vel, sollicitudin eros. Donec vestibulum ante leo, ac tempus sem euismod ut. Integer ac enim metus. Ut quis maximus arcu, vel bibendum sapien. Donec non sollicitudin odio. Praesent faucibus lectus non lectus finibus, et elementum odio scelerisque. Vivamus consectetur id magna sit amet consequat. Sed vitae porttitor massa, vitae laoreet ipsum. Praesent porta ante non urna lobortis laoreet. In ut faucibus turpis, non fringilla metus. Suspendisse potenti. Aliquam dapibus eu elit at auctor. Aliquam tempus ante in tellus pretium cursus. Donec sit amet enim vel neque lacinia fermentum. Nulla auctor neque quis lorem congue, vitae ultrices sem consectetur. Vivamus non sagittis ante. Fusce dui ex, ornare at consectetur nec, blandit at nibh. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas vel est tincidunt, interdum urna ut, volutpat mi. Nullam sed massa faucibus, pellentesque dui sagittis, blandit mauris."; 
+}
+
+function show_news3() {
+    document.getElementById("show_content3").innerHTML = "Phasellus ut diam blandit, sodales purus vel, porttitor sapien. Morbi vel libero vel felis euismod consequat vitae sit amet mauris. Nunc maximus, urna nec dictum molestie, nunc lorem convallis risus, ac bibendum massa dolor eget massa. Vestibulum venenatis tortor mi, sed molestie neque ornare nec. In egestas sagittis lorem. Vestibulum augue mi, viverra ac sem sodales, congue feugiat mi. Vivamus a velit ex. Pellentesque massa metus, bibendum a commodo vitae, tempus vel odio. Vestibulum rutrum lorem quis sapien viverra facilisis. Vestibulum dolor urna, elementum vel egestas id, gravida non massa. Duis dolor risus, tempus nec facilisis quis, fermentum sed urna. "; 
+}
+
+function show_news4() {
+    document.getElementById("show_content4").innerHTML = "Cras non tellus in leo efficitur egestas sed at magna. Sed suscipit pulvinar elit, et sagittis elit. Curabitur pretium arcu nulla, vel viverra tellus rutrum sit amet. Nulla varius lorem congue dolor faucibus pharetra. Mauris efficitur nibh ac enim tempor, in ultricies lacus efficitur. Integer tellus metus, interdum at pretium sit amet, dignissim lobortis nibh. Suspendisse eget lectus nulla. "; 
+}
+
+function show_news5() {
+    document.getElementById("show_content5").innerHTML = "Ut lectus neque, rutrum vel elit a, tempus molestie eros. Integer lobortis lectus vitae neque rutrum, sed commodo elit elementum. Nunc pellentesque magna turpis, sed dictum neque feugiat ut. Integer ligula est, rhoncus sed elit euismod, egestas interdum elit. Sed ipsum elit, venenatis ac purus eget, venenatis tincidunt lectus. Suspendisse vitae elementum ligula. Aenean eu ligula posuere odio lobortis feugiat in sit amet nibh. Integer non leo et purus lacinia ultricies. Cras pellentesque tellus ac felis porta cursus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. "; 
 }
 
 
-
+function show_news6() {
+    document.getElementById("show_content6").innerHTML = "Cras vel vehicula turpis, vel ullamcorper massa. Praesent lobortis nunc ut ligula vulputate dignissim. Curabitur vel nibh porttitor, congue neque vitae, fringilla sapien. Curabitur ullamcorper feugiat sem, eu hendrerit ante rhoncus et. Fusce mi lorem, luctus elementum commodo quis, aliquet nec eros. Duis eu arcu efficitur, elementum diam vitae, maximus sem. Maecenas vulputate, leo nec vehicula cursus, tortor purus efficitur erat, vel tempor mi metus id lacus. Proin venenatis ac ipsum eu porta. "; 
+}
